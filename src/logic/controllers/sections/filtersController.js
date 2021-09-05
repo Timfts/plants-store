1fd2436636fa6c9f1455ed1a719fc8dc79d77e64
Italo Fontes <italo5123@hotmail.com>
2021-09-05 19:25:48 -0300

    :sparkles: added second section layout

--- src/logic/controllers/sections/filtersController.js
+++ src/logic/controllers/sections/filtersController.js
@@ -1,12 +1,29 @@
 import { elementController } from "../../core";
+import { isMobile } from "../../helpers/screen";
 
 const test = {};
 
-export default elementController("section-filters", ({ on }) => {
+export default elementController("section-filters", ({ on, root }) => {
+  const animateClassModifier = "section-filters--animate";
+
   on("selectChange", (e) => {
     test[e?.detail?.slug] = e?.detail?.value;
-    makeRequest()
+    makeRequest();
   });
+
+  (function setupAnimation() {
+    if (!isMobile) {
+      window.addEventListener("load", _onLoadingWindowHandler);
+    }
+  })();
+
+  function _onLoadingWindowHandler() {
+    const timeoutToAnimate = setTimeout(() => {
+      root.classList.add(animateClassModifier);
+      clearTimeout(timeoutToAnimate);
+    }, 1950);
+    window.removeEventListener("load", _onLoadingWindowHandler);
+  }
 
   function makeRequest() {
     console.log(test);
---
+++ src/logic/helpers/display-empty-results.js
@@ -0,0 +1,5 @@
+
+
+export function showEmptyResultsSection(){}
+
+export function hideEmptyResultsSection(){}

f7f40729f6088c079492192a19ad81908a0c7d9b
Italo Fontes <italo5123@hotmail.com>
2021-09-07 15:15:13 -0300

    :sparkles: grid working

--- src/logic/controllers/sections/plantsResultsController.js
+++ src/logic/controllers/sections/plantsResultsController.js
@@ -59,15 +59,52 @@ export default elementController(
     }
 
     /** @param {Plant} plant */
+    function _getIcons(plant) {
+      const toxicIcon = plant.toxicity
+        ? _iconMarkup("ic-toxic")
+        : _iconMarkup("ic-pet-friendly");
+
+      const sunIcon =
+        {
+          no: _iconMarkup("ic-no-sunlight"),
+          low: _iconMarkup("ic-low-sunlight"),
+          high: _iconMarkup("ic-high-sunlight"),
+        }[plant.sun] || "";
+
+      const waterIcon =
+        {
+          regularly: _iconMarkup("ic-regularly-water"),
+          rarely: _iconMarkup("ic-rarely-water"),
+          daily: _iconMarkup("ic-daily-water"),
+        }[plant.water] || "";
+
+      return `
+        ${toxicIcon}
+        ${sunIcon}
+        ${waterIcon}
+      `;
+    }
+
+    function _iconMarkup(id, desc = "") {
+      return `
+      <div class="plant-card__icon" title="${desc}">
+        <svg>
+          <use xlink:href="#${id}"></use>
+        </svg>
+      </div>`;
+    }
+
+    /** @param {Plant} plant */
     function _plantCardMarkup(plant) {
       const isStaffFavorite = !!plant.staffFavorite;
       const mainClass = "plant-card";
       const favoriteClass = isStaffFavorite ? `${mainClass}--favorite` : "";
       const elementClass = `${mainClass} ${favoriteClass}`;
       const title = `<h2 class="plant-card__title">${plant.name}</h2>`;
+      const price = `<p class="plant-card__price">${plant.formattedPrice}</p>`;
 
       return `
-        <div class=${elementClass}>
+        <div class="${elementClass}">
           ${
             isStaffFavorite
               ? '<div class="plant-card__staff-favorite-flag"><h3>✨ Staff favorite</h3></div>'
@@ -81,10 +118,13 @@ export default elementController(
             <div class="plant-card__desc">
               <div class="plant-card__left">
                 ${isStaffFavorite ? title : ""}
-                <p class="plant-card__price">${plant.formattedPrice}</p>
+                ${!isStaffFavorite ? price : ""}
               </div>
               <div class="plant-card__right">
-                
+                ${isStaffFavorite ? price : ""}
+                <div class="plant-card__icons">
+                  ${_getIcons(plant)}
+                </div>
               </div>
             </div>
           </div>

30bf4b790de0de337dbba2c312ebcd59136a777c
Italo Fontes <italo5123@hotmail.com>
2021-09-07 15:54:22 -0300

    :fire: removed unused file

--- src/logic/main.js
+++ src/logic/main.js
@@ -1,11 +1,9 @@
-import bodyController from "./controllers/bodyController";
 import selectController from "./controllers/components/select";
 import heroController from "./controllers/sections/heroController";
 import filtersController from "./controllers/sections/filtersController";
 import plantsResultsController from "./controllers/sections/plantsResultsController";
 
 selectController();
-bodyController();
 heroController();
 filtersController();
 plantsResultsController();
